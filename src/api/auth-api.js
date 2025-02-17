754d3ecf08295af48657432d8adb93f195fd29e0
Walkername <05Timur51@gmail.com>
2025-02-17 18:07:45 +0300

    feat(reg): added register

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -14,15 +14,19 @@ export const login = async (formData) => {
 }
 
 export const register = async (formData) => {
-    const response = await fetch(`${process.env.REACT_APP_USER_SERVICE_URL}/register`, {
+    const response = await fetch(`${process.env.REACT_APP_USER_SERVICE_URL}/auth/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(formData)
     });
+
+    const data = await response.json();
+
     if (!response.ok) {
-        throw new Error('Failed to login');
+        throw new Error(data.message || "Registration failed");
     }
-    return response.json();
+
+    return data;
 }
