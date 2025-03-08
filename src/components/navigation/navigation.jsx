ad708052b578efa66d14702865bc24c35ed63c2a
Walkername <05Timur51@gmail.com>
2025-03-09 00:29:36 +0300

    feat(logout): changed appearance of logout button

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -17,27 +17,27 @@ function NavigationBar() {
     }
 
     const links = [
-        { text: "Main Page", path: "/" },
-        { text: "Profile (in dev.)", path: "/profile" },
-        { text: "Add Movie (Admin)", path: "/movies/add" },
-        { text: "Add User (Admin)", path: "/users/add" }
+        { text: "MOVIE CLUSTER", path: "/" },
+        { text: "Profile", path: "/profile" }
     ];
 
     return (
         <nav>
-            {links.map((link, index) => (
-                <span
-                    className="nav-element"
-                    key={index}
-                    onClick={
-                        () => handleClick(link.path)}
-                >
-                    {link.text}
-                </span>
-            ))}
+            <span className="nav-center">
+                {links.map((link, index) => (
+                    <span
+                        className="nav-element"
+                        key={index}
+                        onClick={
+                            () => handleClick(link.path)}
+                    >
+                        {link.text}
+                    </span>
+                ))}
+            </span>
             {
                 authStatus ?
-                    <button onClick={handleLogout}>Log out</button>
+                    <span className="nav-element logout" onClick={handleLogout}>Log out</span>
                     :
                     <>
                         <span className="nav-element" onClick={() => navigate("/register")}>Register</span>
